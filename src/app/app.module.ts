82d0f3268ec8d22e001b8049ed63125bf1c2505c
Samuel Nadarajan <>
2018-07-24 09:58:45 -0400

    fix module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,43 +1,26 @@
-<<<<<<< HEAD
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import {BrowserModule} from "@angular/platform-browser";
+import {NgModule} from "@angular/core";
 
-import {AppComponent} from './app.component';
+import {AppComponent} from "./app.component";
+import {AngularFireModule} from "angularfire2";
+import {environment} from "../environments/environment";
+import {AngularFirestoreModule} from "angularfire2/firestore";
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireStorageModule} from "angularfire2/storage";
 
 @NgModule({
     declarations: [
         AppComponent
     ],
     imports: [
-        BrowserModule
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        AngularFirestoreModule,
+        AngularFireAuthModule,
+        AngularFireStorageModule
     ],
     providers: [],
     bootstrap: [AppComponent]
-=======
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-
-import { AppComponent } from "./app.component";
-import {AngularFireModule} from "angularfire2";
-import {environment} from "../environments/environment";
-import {AngularFirestoreModule} from 'angularfire2/firestore';
-import {AngularFireAuthModule} from 'angularfire2/auth';
-import {AngularFireStorageModule} from 'angularfire2/storage';
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule,
-    AngularFireAuthModule,
-    AngularFireStorageModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
->>>>>>> 89d37176bfe9e7e5d7c1147ae67e34bd43dec3ee
 })
 export class AppModule {
 }
