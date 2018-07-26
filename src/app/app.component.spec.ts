d45f70d3687c14c8e432e87d69c4938a7ba3dce6
Samuel Nadarajan <>
2018-07-26 16:06:37 -0400

    set up tests for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,28 +1,57 @@
-import {TestBed, async} from '@angular/core/testing';
-import {AppComponent} from './app.component';
+import {TestBed, async} from "@angular/core/testing";
+import {AppComponent} from "./app.component";
+import {SearchComponent} from "./components/search/search.component";
+import {MaterialModule} from "./material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {AngularFirestore, AngularFirestoreModule} from "angularfire2/firestore";
+import {BehaviorSubject} from "rxjs";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
-describe('AppComponent', () => {
+const FirestoreStub = {
+    collection: (name: string) => ({
+        doc: (_id: string) => ({
+            valueChanges: () => new BehaviorSubject({ foo: "bar" }),
+            set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+        }),
+    }),
+};
+
+describe("AppComponent", () => {
+    let fixture, app, compiled;
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [
-                AppComponent
+                AppComponent,
+                SearchComponent
             ],
+            imports: [
+                MaterialModule,
+                FormsModule,
+                ReactiveFormsModule,
+                AngularFirestoreModule,
+                BrowserAnimationsModule
+            ],
+            providers: [
+                { provide: AngularFirestore, useValue: FirestoreStub },
+            ]
         }).compileComponents();
+
+        fixture = TestBed.createComponent(AppComponent);
+        app = fixture.debugElement.componentInstance;
+        compiled = fixture.debugElement.nativeElement;
+
     }));
-    it('should create the app', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
+    it("should create the app", async(() => {
         expect(app).toBeTruthy();
     }));
-    it(`should have as title 'app'`, async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.debugElement.componentInstance;
-        expect(app.title).toEqual('app');
-    }));
-    it('should render title in a h1 tag', async(() => {
-        const fixture = TestBed.createComponent(AppComponent);
+
+    it("should have the correct title", async(() => {
         fixture.detectChanges();
-        const compiled = fixture.debugElement.nativeElement;
-        expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+        expect(compiled.querySelector("h4").textContent.trim()).toEqual("Welcome to " + app.title + "!");
+    }));
+
+    it("should have the search component", async(() => {
+        expect(compiled.querySelector("app-search")).toBeTruthy();
     }));
 });
