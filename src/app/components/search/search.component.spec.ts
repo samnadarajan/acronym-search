6b1b9366cbd1d90730d2a972d2ccd5b8b9ef5469
Samuel Nadarajan <>
2018-08-21 16:52:41 -0400

    and of course tests!

--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -1,11 +1,12 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from "@angular/core/testing";
 
 import { SearchComponent } from "./search.component";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {AngularFirestore, AngularFirestoreModule} from "angularfire2/firestore";
 import {MaterialModule} from "../../material/material.module";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {BehaviorSubject} from "rxjs";
+import {BehaviorSubject, of} from "rxjs";
+import {SearchService} from "../../services/search.service";
 
 const FirestoreStub = {
     collection: (name: string) => ({
@@ -19,6 +20,7 @@ const FirestoreStub = {
 describe("SearchComponent", () => {
     let c: SearchComponent;
     let f: ComponentFixture<SearchComponent>;
+    let compiled;
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
@@ -34,6 +36,7 @@ describe("SearchComponent", () => {
             ],
             providers: [
                 { provide: AngularFirestore, useValue: FirestoreStub },
+                SearchService
             ]
         })
         .compileComponents();
@@ -42,6 +45,7 @@ describe("SearchComponent", () => {
     beforeEach(() => {
         f = TestBed.createComponent(SearchComponent);
         c = f.componentInstance;
+        compiled = f.debugElement.nativeElement;
         f.detectChanges();
     });
 
@@ -54,27 +58,43 @@ describe("SearchComponent", () => {
     });
 
     it("should execute a search when 2+ letters are provided", () => {
-        spyOn(c, "search");
-        expect(c).toBeTruthy();
+        spyOn(c.searchString, "emit");
+        c.beginSearch("test");
+        expect(c.searchString.emit).toHaveBeenCalled();
+
+        c.beginSearch("te");
+        expect(c.searchString.emit).toHaveBeenCalled();
     });
 
-    it("should execute not a search when 2+ letters are provided", () => {
-        expect(c).toBeTruthy();
+    it("should execute not a search when less than 2 letters are provided", () => {
+        spyOn(c.searchString, "emit");
+        c.beginSearch("s");
+        expect(c.searchString.emit).not.toHaveBeenCalled();
     });
 
     it("should have a place to enter an acronym", () => {
-        expect(c).toBeTruthy();
+        expect(compiled.querySelector("mat-form-field input")).toBeTruthy();
     });
 
     it("should show a result", () => {
-        expect(c).toBeTruthy();
+        c.list = of([{code: "TPL", description: "Name of dept", meaning: "Third Party Liability", id: "adsfw23fc234"}]);
+
+        c.list.subscribe(response => {
+            expect(response.length).toEqual(1);
+        });
     });
 
-    it("should show only 1 result", () => {
-        expect(c).toBeTruthy();
-    });
+    it("should show a not found message with no result", fakeAsync(() => {
+        c.list = of([]);
 
-    it("should show a not found message with no result", () => {
-        expect(c).toBeTruthy();
-    });
+        const notFound = compiled.querySelector("span.not-found");
+        expect(notFound).toBeTruthy();
+    }));
+
+    it("should show a not found message with no id", fakeAsync(() => {
+        c.list = of([{code: "TPL", description: "Name of dept", meaning: "Third Party Liability"}]);
+
+        const notFound = compiled.querySelector("span.not-found");
+        expect(notFound).toBeTruthy();
+    }));
 });
