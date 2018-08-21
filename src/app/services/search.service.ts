93686100c6c8637bcbdbd2dd670d6536bb569396
Samuel Nadarajan <>
2018-08-21 16:52:07 -0400

    updating service to use predefined endpoint

--- src/app/services/search.service.ts
+++ src/app/services/search.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from "@angular/core";
-import {AngularFirestore, AngularFirestoreCollection} from "angularfire2/firestore";
+import {AngularFirestore} from "angularfire2/firestore";
 import {Acronym} from "../model/acronym.model";
 import {config} from "../app.config";
 import {map} from "rxjs/operators";
+import {Observable} from "rxjs";
 
 @Injectable({
   providedIn: "root"
 })
 export class SearchService {
-  acronyms: AngularFirestoreCollection<Acronym>;
+  result: Observable<Acronym[]>
 
-  constructor(private db: AngularFirestore) {
-    this.acronyms = this.db.collection<Acronym>(config.collectionEndpoint);
-  }
+  constructor(private db: AngularFirestore) {}
 
   search(code: string) {
-      return this.db.collection("acronyms", ref => ref.where("code", "==", code).limit(1)).snapshotChanges()
+        this.result = this.db.collection(config.collectionEndpoint, ref => ref.where("code", "==", code).limit(1)).snapshotChanges()
           .pipe(map(changes => {
               if (changes.length > 0) {
                   return changes.map(a => {
