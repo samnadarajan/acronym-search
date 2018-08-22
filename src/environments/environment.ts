20b4c8ed1afcf8fd64c650682e517b08e5150080
Samuel Nadarajan <>
2018-08-22 15:57:04 -0400

    remove import

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,6 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-import {firebaseConfigDev} from "../config/firebase.config.dev";
-
 export const environment = {
     production: false,
     firebase: {}
