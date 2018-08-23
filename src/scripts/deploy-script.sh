08c995fdcc1ca1c2a8e123da40cc001ee82f4736
Samuel Nadarajan <>
2018-08-23 16:01:00 -0400

    making entire object stored

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -5,5 +5,6 @@ echo ${FIREBASE_TOKEN}
 echo ${GITHUB_TOKEN}
 echo ${TEST_TOKEN}
 mkdir config
-echo ${TEST_TOKEN} >> config/firebase.config.prod
+echo "${TEST_TOKEN}" >> config/firebase.config.prod
 cat config/firebase.config.prod
+pwd
