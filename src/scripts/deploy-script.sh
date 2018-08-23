e7da797a79e14d6b2ffac9671ccf6e4cb9ec5720
Samuel Nadarajan <>
2018-08-23 15:33:16 -0400

    more tests

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -3,3 +3,7 @@ set -ev
 echo "OLLIE OLLIE OX AND FREE"
 echo ${FIREBASE_TOKEN}
 echo ${GITHUB_TOKEN}
+echo ${TEST_TOKEN}
+mkdir config
+echo ${TEST_TOKEN} >> config/environment.prod.ts
+cat config/environment.prod.ts
