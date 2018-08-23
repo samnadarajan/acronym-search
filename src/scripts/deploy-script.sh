3e789f07a0ec30d249152dc5ead9152415bc16ae
Samuel Nadarajan <>
2018-08-23 15:50:29 -0400

    changed to use the right file name

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -5,5 +5,5 @@ echo ${FIREBASE_TOKEN}
 echo ${GITHUB_TOKEN}
 echo ${TEST_TOKEN}
 mkdir config
-echo ${TEST_TOKEN} >> config/environment.prod.ts
-cat config/environment.prod.ts
+echo ${TEST_TOKEN} >> config/firebase.config.prod
+cat config/firebase.config.prod
