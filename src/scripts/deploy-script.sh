a6b085fb5b03dddef866317835c975af5429a575
Samuel Nadarajan <>
2018-08-28 09:07:36 -0400

    Maybe this one

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -3,7 +3,14 @@ set -ev
 cd src
 mkdir config
 cd config
-echo "${TEST_TOKEN}" >> firebase.config.prod.ts
+echo "export const firebaseConfigProd = {
+          apiKey: \"${FIREBASE_API}\",
+          authDomain: \"${FIREBASE_DOMAIN}\",
+          databaseURL: \"${FIREBASE_DB}\",
+          projectId: \"${FIREBASE_PROJECT_ID}\",
+          storageBucket: \"${FIREBASE_STORAGE_BUCKET}\",
+          messagingSenderId: \"${FIREBASE_SENDER_ID}\"
+      };" >> firebase.config.prod.ts
 
 echo "export const firebaseConfigDev = {
           apiKey: \"\",
