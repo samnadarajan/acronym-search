d07696138f4cc7b58449ac1dffe6d3feb70d5c3e
Samuel Nadarajan <>
2018-08-28 18:29:27 -0400

    trying to add this back

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -7,7 +7,7 @@ echo "export const firebaseConfigProd = {
           apiKey: \"${FIREBASE_API}\",
           authDomain: \"${FIREBASE_DOMAIN}\",
           databaseURL: \"${FIREBASE_DB}\",
-          projectId: \"acronym-search\",
+          projectId: \"${FIREBASE_PROJECT_ID}\",
           storageBucket: \"${FIREBASE_STORAGE_BUCKET}\",
           messagingSenderId: \"${FIREBASE_SENDER_ID}\"
       };" >> firebase.config.prod.ts
