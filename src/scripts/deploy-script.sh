8bdf1becd047657ba7d5a3318464acd0fa4ab881
Samuel Nadarajan <>
2018-08-28 13:04:28 -0400

    forcing deploy

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -7,7 +7,7 @@ echo "export const firebaseConfigProd = {
           apiKey: \"${FIREBASE_API}\",
           authDomain: \"${FIREBASE_DOMAIN}\",
           databaseURL: \"${FIREBASE_DB}\",
-          projectId: \"acronym-search\", 
+          projectId: \"acronym-search\",
           storageBucket: \"${FIREBASE_STORAGE_BUCKET}\",
           messagingSenderId: \"${FIREBASE_SENDER_ID}\"
       };" >> firebase.config.prod.ts
