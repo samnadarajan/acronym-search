b0ba22125ee1180f7bfc376b83fd7bf13a67117c
Samuel Nadarajan <>
2018-08-28 21:12:22 -0400

    building with dev credentials for dev instead of prod

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -13,11 +13,11 @@ echo "export const firebaseConfigProd = {
       };" >> firebase.config.prod.ts
 
 echo "export const firebaseConfigDev = {
-         apiKey: \"${FIREBASE_API}\",
-         authDomain: \"${FIREBASE_DOMAIN}\",
-         databaseURL: \"${FIREBASE_DB}\",
-         projectId: \"${FIREBASE_PROJECT_ID}\",
-         storageBucket: \"${FIREBASE_STORAGE_BUCKET}\",
-         messagingSenderId: \"${FIREBASE_SENDER_ID}\"
+         apiKey: \"${DEV_FIREBASE_API}\",
+         authDomain: \"${DEV_FIREBASE_DOMAIN}\",
+         databaseURL: \"${DEV_FIREBASE_DB}\",
+         projectId: \"${DEV_FIREBASE_PROJECT_ID}\",
+         storageBucket: \"${DEV_FIREBASE_STORAGE_BUCKET}\",
+         messagingSenderId: \"${DEV_FIREBASE_SENDER_ID}\"
      };" >> firebase.config.dev.ts
 
