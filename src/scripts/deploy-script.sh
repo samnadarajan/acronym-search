e60edb5f642b957d4b14449390c92061061b8605
Samuel Nadarajan <>
2018-08-28 23:27:51 -0400

    cleaning up scripts since I'm able to decrypt an encrypted file instead of manually writing variables to different parts of the file

--- src/scripts/deploy-script.sh
+++ src/scripts/deploy-script.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 set -ev
-cd src
-ls
-echo "----"
-ls config/
-mkdir config
-cd config
-echo "export const firebaseConfigProd = {
-          apiKey: \"${FIREBASE_API}\",
-          authDomain: \"${FIREBASE_DOMAIN}\",
-          databaseURL: \"${FIREBASE_DB}\",
-          projectId: \"${FIREBASE_PROJECT_ID}\",
-          storageBucket: \"${FIREBASE_STORAGE_BUCKET}\",
-          messagingSenderId: \"${FIREBASE_SENDER_ID}\"
-      };" >> firebase.config.prod.ts
-
+cd src/config
 echo "export const firebaseConfigDev = {
          apiKey: \"${DEV_FIREBASE_API}\",
          authDomain: \"${DEV_FIREBASE_DOMAIN}\",
