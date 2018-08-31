2bdef2178cd22c96389fe79112dfb0354e5fbc2b
Samuel Nadarajan <>
2018-08-31 12:53:52 -0400

    adding input and output to result

--- src/app/components/result/result.component.ts
+++ src/app/components/result/result.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from "@angular/core";
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output} from "@angular/core";
+import {Observable} from "rxjs";
+import {Acronym} from "../../model/acronym.model";
 
 @Component({
-  selector: "app-result",
-  templateUrl: "./result.component.html",
-  styleUrls: ["./result.component.css"]
+    selector: "app-result",
+    templateUrl: "./result.component.html",
+    styleUrls: ["./result.component.css"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ResultComponent implements OnInit {
+    @Input() result: Observable<Acronym[]>
+    @Output() updatedAcronym = new EventEmitter();
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
