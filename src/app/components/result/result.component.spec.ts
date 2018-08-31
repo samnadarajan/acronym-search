3009c40060deb158ee502bf19791d7e1586f8904
Samuel Nadarajan <>
2018-08-31 13:01:01 -0400

    fixing test

--- src/app/components/result/result.component.spec.ts
+++ src/app/components/result/result.component.spec.ts
@@ -1,25 +1,36 @@
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { ResultComponent } from "./result.component";
+import {ResultComponent} from "./result.component";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MaterialModule} from "../../material/material.module";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularFirestoreModule} from "angularfire2/firestore";
 
 describe("ResultComponent", () => {
-  let component: ResultComponent;
-  let fixture: ComponentFixture<ResultComponent>;
+    let component: ResultComponent;
+    let fixture: ComponentFixture<ResultComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ResultComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ResultComponent],
+            imports: [
+                MaterialModule,
+                FormsModule,
+                ReactiveFormsModule,
+                AngularFirestoreModule,
+                BrowserAnimationsModule
+            ],
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ResultComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ResultComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
