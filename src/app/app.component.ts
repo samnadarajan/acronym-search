348899d40210bfb51511efcdec51998fea2f9aba
Samuel Nadarajan <>
2018-09-10 22:40:56 -0400

    stubbed in an icon for showing the user that is logged in as well as the option to log out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component} from "@angular/core";
+import {AuthService} from "./modules/auth/services/auth/auth.service";
 
 @Component({
     selector: "app-root",
@@ -6,4 +7,5 @@ import {Component} from "@angular/core";
     styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
+    constructor(public authService: AuthService) {}
 }
