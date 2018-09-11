cab018e41ec6eb957582a5b778830ba88f5148b8
Samuel Nadarajan <>
2018-09-11 13:15:55 -0400

    added OnPush

--- src/app/components/acronym/acronym.component.ts
+++ src/app/components/acronym/acronym.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {SearchService} from "../../services/search/search.service";
 
 @Component({
-  selector: "app-acronym",
-  templateUrl: "./acronym.component.html",
-  styleUrls: ["./acronym.component.css"]
+    selector: "app-acronym",
+    templateUrl: "./acronym.component.html",
+    styleUrls: ["./acronym.component.css"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AcronymComponent {
     title = "Acronym Search";
