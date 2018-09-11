6da3e2b46907deeb7465e1398e41c48838503ce2
Samuel Nadarajan <>
2018-09-11 13:15:43 -0400

    corrected test

--- src/app/components/acronym/acronym.component.spec.ts
+++ src/app/components/acronym/acronym.component.spec.ts
@@ -1,25 +1,66 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { AcronymComponent } from './acronym.component';
+import {AcronymComponent} from "./acronym.component";
+import {SearchComponent} from "../search/search.component";
+import {ResultComponent} from "../result/result.component";
+import {MaterialModule} from "../../material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularFirestoreModule, AngularFirestore} from "@angular/fire/firestore";
+import {BehaviorSubject} from "rxjs";
 
-describe('AcronymComponent', () => {
-  let component: AcronymComponent;
-  let fixture: ComponentFixture<AcronymComponent>;
+const FirestoreStub = {
+    collection: (name: string) => ({
+        doc: (_id: string) => ({
+            valueChanges: () => new BehaviorSubject({ foo: "bar" }),
+            set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+        }),
+    }),
+};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AcronymComponent ]
-    })
-    .compileComponents();
-  }));
+describe("AcronymComponent", () => {
+    let component: AcronymComponent;
+    let fixture: ComponentFixture<AcronymComponent>;
+    let compiled;
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AcronymComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                AcronymComponent,
+                SearchComponent,
+                ResultComponent
+            ],
+            imports: [
+                MaterialModule,
+                FormsModule,
+                ReactiveFormsModule,
+                AngularFirestoreModule,
+                BrowserAnimationsModule
+            ],
+            providers: [
+                { provide: AngularFirestore, useValue: FirestoreStub },
+            ]
+        })
+            .compileComponents();
+    }));
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AcronymComponent);
+        component = fixture.componentInstance;
+        compiled = fixture.debugElement.nativeElement;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+
+    it("should have the correct title", async(() => {
+        fixture.detectChanges();
+        expect(compiled.querySelector("h4").textContent.trim()).toEqual("Welcome to " + component.title + "!");
+    }));
+
+    it("should have the search component", async(() => {
+        expect(compiled.querySelector("app-search")).toBeTruthy();
+    }));
 });
