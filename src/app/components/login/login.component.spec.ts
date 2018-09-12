d3717b557ca3ac45cec8c0b6947e548975580281
Samuel Nadarajan <>
2018-09-11 20:30:16 -0400

    fixed tests for login component

--- src/app/components/login/login.component.spec.ts
+++ src/app/components/login/login.component.spec.ts
@@ -1,25 +1,65 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from "@angular/core/testing";
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from "./login.component";
+import {MaterialModule} from "../../material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {AngularFirestoreModule, AngularFirestore} from "@angular/fire/firestore";
+import {AuthModule} from "../../modules/auth/auth.module";
+import {BehaviorSubject, Observable, of} from "rxjs";
+import {AuthService} from "../../modules/auth/services/auth/auth.service";
+import {AngularFireAuth} from "@angular/fire/auth";
+import {RouterTestingModule} from "@angular/router/testing";
 
-describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+const FirestoreStub = {
+    collection: (name: string) => ({
+        doc: (_id: string) => ({
+            valueChanges: () => new BehaviorSubject({ foo: "bar" }),
+            set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+        }),
+    }),
+};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
-  }));
+const FireAuthStub = {
+    auth: (name: string) => ({
+        signInWithPopup: (provider: any) => new Promise((resolve, _reject) => resolve()),
+        signOut: () => new Promise((resolve, _reject) => resolve())
+    }),
+    authState: of({email: "test@test.com", password: "password"})
+}
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+describe("LoginComponent", () => {
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            imports: [
+                MaterialModule,
+                FormsModule,
+                ReactiveFormsModule,
+                AngularFirestoreModule,
+                BrowserAnimationsModule,
+                AuthModule,
+                RouterTestingModule
+            ],
+            providers: [
+                { provide: AngularFirestore, useValue: FirestoreStub },
+                { provide: AngularFireAuth, useValue: FireAuthStub },
+                AuthService
+            ]
+        })
+            .compileComponents();
+    }));
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,16 +1,12 @@
-import { Component, OnInit } from "@angular/core";
+import {ChangeDetectionStrategy, Component} from "@angular/core";
 import {AuthService} from "../../modules/auth/services/auth/auth.service";
 
 @Component({
-  selector: "app-login",
-  templateUrl: "./login.component.html",
-  styleUrls: ["./login.component.css"]
+    selector: "app-login",
+    templateUrl: "./login.component.html",
+    styleUrls: ["./login.component.css"],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LoginComponent implements OnInit {
-
-  constructor(public authService: AuthService) { }
-
-  ngOnInit() {
-  }
-
+export class LoginComponent {
+    constructor(public authService: AuthService) {}
 }
