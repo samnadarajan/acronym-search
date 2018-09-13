2b695d8930e1438e0edee162f1b965c756ba8c6a
Samuel Nadarajan <>
2018-09-12 23:38:13 -0400

    added a new project component

---
+++ src/app/components/project-select/project-select.component.ts
@@ -0,0 +1,24 @@
+import {ChangeDetectionStrategy, Component, EventEmitter, Input, Output} from "@angular/core";
+import {Observable} from "rxjs";
+import {Project} from "../../model/project.model";
+import {FormControl} from "@angular/forms";
+import {ProjectService} from "../../services/project/project.service";
+
+@Component({
+    selector: "app-project-select",
+    templateUrl: "./project-select.component.html",
+    styleUrls: ["./project-select.component.css"],
+    changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ProjectSelectComponent {
+    @Input() projects: Observable<Project[]>;
+    @Output() selectedProject = new EventEmitter();
+
+    chosenProject = new FormControl("");
+
+    constructor(private _projectService: ProjectService) {}
+
+    onChange(project) {
+        this._projectService.setProject(project);
+    }
+}
