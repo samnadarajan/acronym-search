2375eb50a10051254f491dc55978da8270893d0f
Samuel Nadarajan <>
2018-09-12 23:45:26 -0400

    adding providers and setup for new component

--- src/app/components/project-select/project-select.component.spec.ts
+++ src/app/components/project-select/project-select.component.spec.ts
@@ -1,14 +1,44 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { ProjectSelectComponent } from './project-select.component';
+import { ProjectSelectComponent } from "./project-select.component";
+import {BehaviorSubject} from "rxjs";
+import {MaterialModule} from "../../material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {SearchService} from "../../services/search/search.service";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {NgSelectModule} from "../../../../node_modules/@ng-select/ng-select";
+import {AngularFirestoreModule, AngularFirestore} from "../../../../node_modules/@angular/fire/firestore";
 
-describe('ProjectSelectComponent', () => {
+const FirestoreStub = {
+    collection: (name: string) => ({
+        doc: (_id: string) => ({
+            valueChanges: () => new BehaviorSubject({ foo: "bar" }),
+            set: (_d: any) => new Promise((resolve, _reject) => resolve()),
+        }),
+    }),
+};
+
+describe("ProjectSelectComponent", () => {
   let component: ProjectSelectComponent;
   let fixture: ComponentFixture<ProjectSelectComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProjectSelectComponent ]
+        declarations: [
+            ProjectSelectComponent
+        ],
+        imports: [
+            MaterialModule,
+            FormsModule,
+            ReactiveFormsModule,
+            AngularFirestoreModule,
+            BrowserAnimationsModule,
+            NgSelectModule
+        ],
+        providers: [
+            { provide: AngularFirestore, useValue: FirestoreStub },
+            SearchService
+        ]
     })
     .compileComponents();
   }));
@@ -19,7 +49,7 @@ describe('ProjectSelectComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
