1fb280322c24ed2a4aa5f01217753bb79e58fed7
Samuel Nadarajan <>
2018-09-19 21:30:45 -0400

    changing app state for projects

--- src/app/app.state.ts
+++ src/app/app.state.ts
@@ -1,7 +1,7 @@
 import {Acronym} from "./model/acronym.model";
-import {Project} from "./model/project.model";
+import {Projects} from "./model/projects.model";
 
 export interface AppState {
     readonly acronym: Acronym;
-    readonly projects: Project[];
+    readonly projects: Projects;
 }
