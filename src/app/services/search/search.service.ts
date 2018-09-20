51078f23fb089be0d1fa27d0fbfb2e1c22c6c902
Samuel Nadarajan <>
2018-09-19 21:27:58 -0400

    service now uses observables properly for the project

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -8,6 +8,7 @@ import {ProjectService} from "../project/project.service";
 import {AppState} from "../../app.state";
 import {Store} from "../../../../node_modules/@ngrx/store";
 import * as AcronymActions from "../../actions/acronym.actions";
+import {Project} from "../../model/project.model";
 
 @Injectable({
   providedIn: "root"
@@ -17,12 +18,12 @@ export class SearchService {
     searched = false;
     constructor(private db: AngularFirestore, private projectService: ProjectService, private _store: Store<AppState>) {}
 
-    search(code: string) {
+    search(code: string, project: Project) {
         this.searched = true;
         this.db.collection(
             config.acronyms,
                 ref => ref.where("code", "==", code)
-                                    .where("project", "==", this.projectService.currentProject.name)
+                                    .where("project", "==", project.name)
                     .limit(1))
             .snapshotChanges()
           .pipe(map(changes => {
@@ -40,20 +41,21 @@ export class SearchService {
             });
     }
 
-    save(acronym: Acronym) {
+    save(acronym: Acronym, project: Project) {
         if (acronym.id) {
             this.update(acronym);
         } else {
+            acronym.project = project.name;
             this.add(acronym);
         }
     }
 
     add(acronym: Acronym) {
-        this.db.collection(config.acronyms).add(acronym).then(() => {this.search(acronym.code); });
+        this.db.collection(config.acronyms).add(acronym);
     }
 
     update(acronym: Acronym) {
-        this.db.collection(config.acronyms).doc(acronym.id).update(acronym).then(() => { this.search(acronym.code); });
+        this.db.collection(config.acronyms).doc(acronym.id).update(acronym);
     }
 
 
