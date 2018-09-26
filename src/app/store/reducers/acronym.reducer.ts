9b350c4c80807bf151cffc3554e7ff7de19d5ee1
Samuel Nadarajan <>
2018-09-25 23:07:43 -0400

    updating reducer for saving acronyms

--- src/app/store/reducers/acronym.reducer.ts
+++ src/app/store/reducers/acronym.reducer.ts
@@ -1,27 +1,30 @@
 import * as AcronymActions from "../actions/acronym.actions";
-import {storeLogger} from "ngrx-store-logger";
 import {Acronym} from "../../model/acronym.model";
 
 export interface AcronymState {
     acronym: Acronym;
+    loading: boolean;
+    loaded: boolean;
 }
 export const initialState: AcronymState = {
-    acronym: {code: ""}
+    acronym: {code: ""},
+    loading: false,
+    loaded: true
 };
 
 export function acronymReducer(state = initialState, action: AcronymActions.AcronymActions) {
     switch (action.type) {
         case AcronymActions.SEARCH_ACRONYM:
-        // case AcronymActions.UPDATE_ACRONYM:
-        // case AcronymActions.ADD_ACRONYM:
+        case AcronymActions.SAVE_ACRONYM:
             // storeLogger({level: "info"});
-            return {...state, acronym: action.payload};
+            return {...state, loading: true, loaded: false};
+        case AcronymActions.SAVE_ACRONYM_SUCCESS:
         case AcronymActions.SEARCH_ACRONYM_SUCCESS:
-            return {...state, loading: false, loaded: true};
+            return {...state, acronym: action.payload, loaded: true};
+        case AcronymActions.SAVE_ACRONYM_FAIL:
         case AcronymActions.SEARCH_ACRONYM_FAIL:
-            return {...state, loading: false, loaded: false};
+            return {...state, loading: false, loaded: true};
         default:
-            // storeLogger({level: "info"});
             return state;
     }
 }
