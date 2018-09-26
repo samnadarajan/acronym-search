bde0fddef459858eb590fd25e0eca7b12e4c515a
Samuel Nadarajan <>
2018-09-25 23:09:20 -0400

    removing unnecessary methods for projects for right now

--- src/app/services/project/project.service.ts
+++ src/app/services/project/project.service.ts
@@ -3,16 +3,15 @@ import {map} from "rxjs/operators";
 import {config} from "../../app.config";
 import {AngularFirestore} from "../../../../node_modules/@angular/fire/firestore";
 import {Project} from "../../model/project.model";
-import {AppState} from "../../app.state";
+import {AppState} from "../../store/app.state";
 import {Store} from "../../../../node_modules/@ngrx/store";
-import * as ProjectActions from "../../actions/project.actions";
+import * as ProjectActions from "../../store/actions/project.actions";
 
 @Injectable({
   providedIn: "root"
 })
 export class ProjectService {
-    currentProject: Project;
-    constructor(private db: AngularFirestore, private _store: Store<AppState>) { }
+    constructor(private db: AngularFirestore, private _store: Store<AppState>) {}
 
     getProjects() {
         this.db.collection(config.projects).snapshotChanges()
@@ -29,21 +28,5 @@ export class ProjectService {
           })).subscribe(response => {
               this._store.dispatch(new ProjectActions.LoadProjects(response));
             });
-    }
-
-    save(project: Project) {
-        if (project.id) {
-            this.update(project);
-        } else {
-            this.add(project);
-        }
-    }
-
-    add(project: Project) {
-        this.db.collection(config.projects).add(project);
-    }
-
-    update(project: Project) {
-        this.db.collection(config.projects).doc(project.id).update(project);
     }
 }
