8f41e871f958b36589c0b5efb7fb3468fab2d4da
Samuel Nadarajan <>
2018-09-25 20:32:27 -0400

    cleaned up search service

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from "@angular/core";
 import {map} from "rxjs/operators";
-import {Observable} from "rxjs";
+import {Observable, of} from "rxjs";
 import {AngularFirestore} from "@angular/fire/firestore";
 import {Acronym} from "../../model/acronym.model";
 import {config} from "../../app.config";
-import {ProjectService} from "../project/project.service";
 import {AppState} from "../../store/app.state";
 import {Store} from "../../../../node_modules/@ngrx/store";
 import * as AcronymActions from "../../store/actions/acronym.actions";
@@ -16,31 +15,15 @@ import {Project} from "../../model/project.model";
 export class SearchService {
     result: Observable<Acronym[]>;
     searched = false;
-    constructor(private db: AngularFirestore, private projectService: ProjectService, private _store: Store<AppState>) {}
+    constructor(private db: AngularFirestore) {}
 
-    search(payload: any) {
-        const code = payload.code;
-        const project = payload.project;
-        // this.searched = true;
+    search(code: string, projectName: string): Observable<any> {
         return this.db.collection(
             config.acronyms,
                 ref => ref.where("code", "==", code)
-                                    .where("project", "==", project.name)
-                    .limit(1));
-          //   .snapshotChanges()
-          // .pipe(map(changes => {
-          //     if (changes.length > 0) {
-          //         return changes.map(a => {
-          //             const data = a.payload.doc.data() as Acronym;
-          //             data.id = a.payload.doc.id;
-          //             return data;
-          //         });
-          //     } else {
-          //         return [{code: code} as Acronym];
-          //     }
-          // })).subscribe(response => {
-          //     this._store.dispatch(new AcronymActions.SearchAcronym(response[0]));
-          //   });
+                    .where("project", "==", projectName)
+                    .limit(1))
+            .snapshotChanges();
     }
 
     save(acronym: Acronym, project: Project) {
