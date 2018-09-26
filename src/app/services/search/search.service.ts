cb63bc68b511dba39682d97577255f77f8b1ab13
Samuel Nadarajan <>
2018-09-25 23:04:22 -0400

    cleaned up the service method to just save and search

--- src/app/services/search/search.service.ts
+++ src/app/services/search/search.service.ts
@@ -1,13 +1,8 @@
 import { Injectable } from "@angular/core";
-import {map} from "rxjs/operators";
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
 import {AngularFirestore} from "@angular/fire/firestore";
 import {Acronym} from "../../model/acronym.model";
 import {config} from "../../app.config";
-import {AppState} from "../../store/app.state";
-import {Store} from "../../../../node_modules/@ngrx/store";
-import * as AcronymActions from "../../store/actions/acronym.actions";
-import {Project} from "../../model/project.model";
 
 @Injectable({
   providedIn: "root"
@@ -26,21 +21,20 @@ export class SearchService {
             .snapshotChanges();
     }
 
-    save(acronym: Acronym, project: Project) {
+    save(acronym: Acronym) {
         if (acronym.id) {
-            this.update(acronym);
+            return this.update(acronym);
         } else {
-            acronym.project = project.name;
-            this.add(acronym);
+            return this.add(acronym);
         }
     }
 
     add(acronym: Acronym) {
-        this.db.collection(config.acronyms).add(acronym);
+        return this.db.collection(config.acronyms).add(acronym);
     }
 
     update(acronym: Acronym) {
-        this.db.collection(config.acronyms).doc(acronym.id).update(acronym);
+        return this.db.collection(config.acronyms).doc(acronym.id).update(acronym);
     }
 
 
