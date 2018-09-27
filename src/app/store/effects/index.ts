7003a9fae5a20ae8bc606829c942d8d2484743b5
Samuel Nadarajan <>
2018-09-26 22:22:03 -0400

    barrelling for projects

--- src/app/store/effects/index.ts
+++ src/app/store/effects/index.ts
@@ -1,5 +1,7 @@
 import {AcronymEffect} from "./acronym.effect";
+import {ProjectEffect} from "./project.effect";
 
-export const effects: any[] = [AcronymEffect];
+export const effects: any[] = [AcronymEffect, ProjectEffect];
 
 export * from "./acronym.effect";
+export * from "./project.effect";
