24edb3551c4f7c72275cb898a2fbaf0bb9aae7b5
Samuel Nadarajan <>
2018-11-04 18:23:52 -0500

    fixed tests for the modal

--- src/app/components/project-modal/project-modal.component.spec.ts
+++ src/app/components/project-modal/project-modal.component.spec.ts
@@ -1,25 +1,40 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { ProjectModalComponent } from "./project-modal.component";
+import {MaterialModule} from "../../material/material.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {MAT_DIALOG_DATA} from "@angular/material";
+import {StoreModule} from "@ngrx/store";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 describe("ProjectModalComponent", () => {
-  let component: ProjectModalComponent;
-  let fixture: ComponentFixture<ProjectModalComponent>;
+    let component: ProjectModalComponent;
+    let fixture: ComponentFixture<ProjectModalComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ProjectModalComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ ProjectModalComponent ],
+            imports: [
+                MaterialModule,
+                FormsModule,
+                ReactiveFormsModule,
+                BrowserAnimationsModule,
+                StoreModule.forRoot({})
+            ],
+            providers: [
+                {provide: MAT_DIALOG_DATA, useValue: {}}
+            ]
+        })
+        .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProjectModalComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProjectModalComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
