4892c7b25587cc5f970c3cef2d0dc19b534c4328
Samuel Nadarajan <>
2018-11-04 11:38:45 -0500

    specs for actions for auth user

---
+++ src/app/store/actions/auth-user.actions.spec.ts
@@ -0,0 +1,18 @@
+import * as fromActions from "@app/store/actions/authUser.actions";
+
+describe("Auth User Actions", () => {
+    it("should create the Login action", () => {
+        const user = {uid: "2sdfafe32", displayName: "Sam Nadarajan", email: "test@test.com"};
+
+        const action = new fromActions.Login(user);
+
+        expect(action.type).toEqual(fromActions.LOGIN_USER);
+        expect(action.payload).toEqual(user);
+    });
+
+    it("should create the Logout action", () => {
+        const action = new fromActions.Logout();
+
+        expect(action.type).toEqual(fromActions.LOGOUT_USER);
+    });
+});
