2353a876e530ed6597ba52abdbe7b566b05d443e
Samuel Nadarajan <>
2018-11-12 21:46:54 -0500

    switching from valueChanges to snapshotChanges so I can get metadata

--- src/app/services/project/project.service.spec.ts
+++ src/app/services/project/project.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from "@angular/core/testing";
 
 import {ProjectService} from "./project.service";
-import {AngularFirestore} from "../../../../node_modules/@angular/fire/firestore";
+import {AngularFirestore} from "@angular/fire/firestore";
 import {Store, StoreModule} from "@ngrx/store";
-import {config} from "../../app.config";
+import {config} from "@app/app.config";
 
 const collectionStub = {
-    valueChanges: jasmine.createSpy("snapshotChanges").and.returnValue({code: "SAM"})
+    valueChanges: jasmine.createSpy("valueChanges").and.returnValue({code: "SAM"}),
+    snapshotChanges: jasmine.createSpy("snapshotChanges").and.returnValue({code: "SAM"})
 };
 
 const firestoreStub = {
